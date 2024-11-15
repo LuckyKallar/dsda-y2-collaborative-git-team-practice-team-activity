b05b4e4931ee7a06466c38f11637b6bbaa63119f
LuckyKallar <94996647+LuckyKallar@users.noreply.github.com>
2024-11-15 14:46:13 +0000

    Pair 2 changes updated

--- Pair 2 (Calculating squares).py
+++ Pair 2 (Calculating squares).py
@@ -1,10 +1,17 @@
-def calculate_square(number):
-    """Calculates the square of a number."""
-    return number * number
 
+# Method to calculate the square of a number
+def calculate_to_the_power(number, powerof=2):
+    return number**powerof
+
+# user interaction to get the result of increase by the power of x
 if __name__ == "__main__":
-    user_number = int(input("Enter a number: "))
-    result = calculate_square(user_number)
-    print(f"The square of {user_number} is {result}")
 
-#number 2 file
+    # Ask the user for the numbers to calculate
+    user_number = int(input("Enter a number: "))
+    powerof = int(input("Enter a the power: "))
+
+    #calculate the result
+    result = calculate_to_the_power(user_number, powerof)
+
+    #print the result
+    print(f"The square of {user_number} to the power of {powerof} is {result}")
